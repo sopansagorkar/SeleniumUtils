980201a6669d1a8d2df36cf9926a6de6c45720db
Sopan sagorkar <sopansa@cybage.com>
2018-03-15 10:34:33 +0530

    Artifact added

--- src/test/java/com/cybage/selenium/SeleniumUtils/Barclays.java
+++ src/test/java/com/cybage/selenium/SeleniumUtils/Barclays.java
@@ -5,7 +5,7 @@ public class Barclays {
 	public static void main(String a[]) {
 		int b = 5;
 		int c = 6;
-		System.out.println(b + " " + c);
+		System.out.println("b= " + b + " " + "c= " + c);
 		Barclays barclays = new Barclays();
 		barclays.swapNumber(b, c);
 	}
@@ -14,8 +14,7 @@ public class Barclays {
 		b = b + c;
 		c = b - c;
 		b = b - c;
-		System.out.println(b + " " + c);
+		System.out.println("b= " + b + " " + "c= " + c);
 	}
-	
 
 }
